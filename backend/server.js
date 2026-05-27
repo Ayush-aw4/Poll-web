f23fe1033842cf41bce5ad2f37a2c88eb8f8494c
AYUSH AWCHAR <ayushawchar9403@gmail.com>
2026-05-27 18:24:16 +0530

    production

--- backend/server.js
+++ backend/server.js
@@ -5,41 +5,43 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const pollRoutes = require('./routes/pollRoutes');
 const { errorHandler } = require('./middleware/errorHandler');
+const path = require("path");
 
-// Load environment variables
 dotenv.config();
 
-// Connect to Database
 connectDB();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
+// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/polls', pollRoutes);
 
-// Simple healthcheck route
-app.get('/', (req, res) => {
-  res.json({ message: 'Poll/Voting API is running...' });
-});
+// Serve frontend
+if(process.env.NODE_ENV === "production"){
 
-// 404 Route handler
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
+
+// 404 handler
 app.use((req, res, next) => {
   res.status(404);
   const error = new Error(`Not Found - ${req.originalUrl}`);
   next(error);
 });
 
-// Global Error Handler Middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
